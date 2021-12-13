6e2b27d2d4ece67e05e029eeeb2c9e610b5c8432
Shmuel Meymann <smeymann@lightricks.com>
2021-12-13 22:51:04 +0200

    Comment

--- app/src/main/java/com/playgrounds/projectsapiens/model/PositionResolver.java
+++ app/src/main/java/com/playgrounds/projectsapiens/model/PositionResolver.java
@@ -1,5 +1,8 @@
 package com.playgrounds.projectsapiens.model;
 
+/**
+ * A Position Resolver is a class that determines what kind of item should reside in each position.
+ */
 public interface PositionResolver {
     int get(int position);
 }
